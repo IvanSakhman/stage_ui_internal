6f09ec9764782d816c57b171069bdb93c95b8d0d
Nataliia Khoverko <khoverko@gmail.com>
2024-03-20 15:18:31 +0200

    add check to the usePusher hook, updated test:debug script

--- src/lib/hooks/usePusher.js
+++ src/lib/hooks/usePusher.js
@@ -56,7 +56,7 @@ export default ({ triggerGlobalAlert, removeGlobalAlert, setUIStoreProperty, web
         })
 
         return () => {
-          pusherclient.unbind_all()
+          pusherclient.unbind_all?.()
         }
       }
     }, [pusherclient])
