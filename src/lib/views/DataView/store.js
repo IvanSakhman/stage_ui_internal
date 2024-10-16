620088a2894fc5f4e91d23fab50d6576ed4ac0e1
Veronika <veronika.tsebriy@gmail.com>
2024-10-16 12:42:31 +0300

    update packages and zustand import to v.5

--- src/lib/views/DataView/store.js
+++ src/lib/views/DataView/store.js
@@ -1,4 +1,4 @@
-import { create } from 'zustand'
+import { createWithEqualityFn as create } from 'zustand/traditional'
 import { shallow } from 'zustand/shallow'
 import { buildCollectionState } from '~su/actions'
 
