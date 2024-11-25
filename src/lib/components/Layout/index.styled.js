62b4a9aaa17c818bbab8e5e649a4f197f335e320
Nika202021 <52848897+Nika202021@users.noreply.github.com>
2024-11-25 11:01:23 +0200

    make min-width only on mobile

--- src/lib/components/Layout/index.styled.js
+++ src/lib/components/Layout/index.styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { Layout } from 'antd'
+import { MEDIA } from '~su/constants/mediaQueries'
 const { Content } = Layout
 
 export const StyledContent = styled(Content)`
@@ -11,5 +12,9 @@ export const StyledContent = styled(Content)`
 // colors should be rewritten in scope of https://assembly-tech.atlassian.net/browse/SC-2
 export const StyledLayout = styled(Layout)`
   min-height: 100vh;
-  min-width: 81.25rem;
+  min-width: 1576px;
+
+  ${MEDIA.screen.lg} {
+    min-width: initial;
+  }
 `
